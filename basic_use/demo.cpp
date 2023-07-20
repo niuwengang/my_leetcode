0135abbdfd2e5e39a5c65504cfd1140486b5a821
niu_wengang <niu_wengang@163.com>
2023-07-20 08:52:15 +0800

    update

---
+++ basic_use/demo.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+void MYe(char *file = __FILE__, unsigned long line = __LINE__)
+{
+    std::cout << file << std::endl;
+    std::cout << line << std::endl;
+}
+
+int main()
+{
+    MYe();
+    return 0;
+}
