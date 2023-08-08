eb995332e2f127fc8f196e923b595cbd1c0ad261
niu_wengang <niu_wengang@163.com>
2023-08-09 01:31:41 +0800

    181

---
+++ everyday/1281整数的各位积和之差.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution
+{
+  public:
+    int subtractProductAndSum(int n)
+    {
+        string str = to_string(n);
+        const int len = str.size();
+        int product = 1, sum = 0;
+        for (int i = 0; i < len; i++)
+        {
+            int num = str[i] - '0';
+            product *= num;
+            sum += num;
+        }
+        return (product - sum);
+    }
+};
+
+int main()
+{
+    shared_ptr<Solution> solve_ptr = std::make_shared<Solution>();
+    solve_ptr->subtractProductAndSum(244);
+     return 0;
+}
